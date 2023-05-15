1b2687d93b5e7ddc226a062cf9e67f36cfa530c0
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-05-15 12:21:47 +0530

    1 file added

---
+++ src/app/shared/pipe/notification-pipe.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'notificationPipe'
+})
+export class NotificationPipePipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    return null;
+  }
+
+}
