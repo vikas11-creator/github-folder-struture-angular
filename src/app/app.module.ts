3d61b2b562c35ead5feb8170a1e0a35e55b41749
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-05-15 21:00:28 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,22 @@ import { coreComponents } from './common-component';
 import { rootPages } from './pages';
 import { AppSharedModule } from './shared/shared.module';
 
-export function loadUserInfo() {
-  console.log(
-    'APP_INITIALIZER:-',
-    'this will be logged before page initiallization'
-  );
-}
+// export function loadUserInfo() {
+//   console.log(
+//     "APP_INITIALIZER:-",
+//     'this will be logged before page initiallization'
+//   );
+// }
 @NgModule({
   declarations: [AppComponent, ...coreComponents, ...rootPages],
   imports: [BrowserModule, AppRoutingModule, AppSharedModule],
   providers: [
-    {
-      provide: APP_INITIALIZER, //app initialize hone se pehle call krega
-      useFactory: loadUserInfo, //this is the function jo call krega
-      multi: true,
-      deps: [], //dependency yha dalo jo function me use hone wala h
-    },
+    // {
+    //   provide: APP_INITIALIZER, //app initialize hone se pehle call krega
+    //   useFactory: loadUserInfo, //this is the function jo call krega
+    //   multi: true,
+    //   deps: [], //dependency yha dalo jo function me use hone wala h
+    // },
   ],
   bootstrap: [AppComponent],
 })
