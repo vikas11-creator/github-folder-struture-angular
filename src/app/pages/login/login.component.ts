9143276db7646e287bc6b39a8b75caee32b6eade
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-05-15 21:16:24 +0530

    1 file modified

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,18 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  height: any = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // height: any = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   constructor() {}
 
   ngOnInit(): void {
-    console.log(this.height);
-    let i = 0;
-    // let max = 0;
-    let store;
-    console.log(this.height);
-    for (let j = this.height.length - 1; j < this.height.length; j--) {
-      store = Math.min(this.height[i], this.height[j]) * Math.abs(j - i);
-      // console.log(store);
-    }
+    // console.log(this.height);
+    // let i = 0;
+    // // let max = 0;
+    // let store;
+    // console.log(this.height);
+    // for (let j = this.height.length - 1; j < this.height.length; j--) {
+    //   store = Math.min(this.height[i], this.height[j]) * Math.abs(j - i);
+    //   // console.log(store);
+    // }
   }
 }
