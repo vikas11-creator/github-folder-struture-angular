57c7a6371012ffeb0e2865a62c474cedc9037ae6
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-07-12 22:40:39 +0530

    2 files modified

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { BaseClass } from './../../shared/common/baseClass';
+import { Component, OnInit, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  password = 'password';
+  isPassword: boolean = true;
+  userData: any = {};
+  strMsg: string;
+  errorMessage: string = '';
+  formSubmitAttempt: boolean = false;
+  iconShow: boolean = false;
+  isBadCredential: boolean = false;
+  loginForm: FormGroup;
 
-  constructor() { }
+  public validation_messages = {
+    'username': [
+      { type: 'required', message: 'Please enter username' },
+      { type: 'maxlength', message: 'username must be between 2 to 50 characters' },
+      { type: 'minlength', message: 'username must be between 2 to 50 characters' },
 
-  ngOnInit(): void {
+    ],
+    'password': [
+      { type: 'required', message: 'Please enter password' },
+      { type: 'minlength', message: 'password must be between 5 to 50 characters' }
+    ]
+  };
+  constructor(private fb: FormBuilder,
+    public override injector: Injector,) {
+    super(injector);
+    this.strMsg = '';
   }
 
+  ngOnInit(): void {
+   this.initializeLoginForm();
+  }
+
+  initializeLoginForm() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.compose([Validators.required,Validators.minLength(2), Validators.maxLength(51)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(5)])]
+    });
+  }
+
+  login(loginForm: FormGroup) {
+    this.formSubmitAttempt = true;
+  }
 }
