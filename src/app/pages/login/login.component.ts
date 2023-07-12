52b36c0e91a262dc6e75778ab66d2759b4376ce1
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-07-12 22:00:02 +0530

    2 files modified

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,23 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { BaseClass } from './../../shared/common/baseClass';
+import { Component, OnInit, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { PATH } from 'src/app/app.constant';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { VALIDATION_PATTERNS } from 'src/app/shared/common/validation-patterns';
+import { sharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-  // height: any = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  constructor() {}
+export class LoginComponent extends BaseClass implements OnInit {
+  password = 'password';
+  isPassword: boolean = true;
+  userData: any = {};
+  strMsg: string;
+  errorMessage: string = '';
+  formSubmitAttempt: boolean = false;
+  iconShow: boolean = false;
+  isBadCredential: boolean = false;
+  
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private spinnerService: NgxSpinnerService,
+    private authService: AuthService,
+    public override injector: Injector,
+    private sharedService: sharedService) {
+    super(injector);
+    this.strMsg = '';
+  }
 
   ngOnInit(): void {
-    // console.log(this.height);
-    // let i = 0;
-    // // let max = 0;
-    // let store;
-    // console.log(this.height);
-    // for (let j = this.height.length - 1; j < this.height.length; j--) {
-    //   store = Math.min(this.height[i], this.height[j]) * Math.abs(j - i);
-    //   // console.log(store);
-    // }
+   
   }
 }
